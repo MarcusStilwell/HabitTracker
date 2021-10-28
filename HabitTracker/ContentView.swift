b4799f08f7d4592d138522719d171564c8d70f82
MarcusStilwell <marcusstilwell@icloud.com>
2021-10-28 16:00:41 -0700

    Initial Commit

---
+++ HabitTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HabitTracker
+//
+//  Created by Marcus Stilwell on 10/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
